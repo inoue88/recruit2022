40af698daf5bf91292fc9421561fe27363a90763
inoue <inoue@inouenoMacBook-Pro-2.local>
2020-01-17 15:22:02 +0900

    質問事項の修正

--- src/views/Member.vue
+++ src/views/Member.vue
@@ -16,16 +16,14 @@
   b-container(v-if='item')
       .interview-content
         section.interview-content__section(v-for='(n,index) in 3')
-          h3.interview-content__q
-            |{{question[index]}}
+          h3.interview-content__q(v-html="item.question[`${index}`]")
           .interview-content__a(v-html="item.answer[`${index}`]")
   .interview-image01
     b-img(v-bind:src="`/static/images/member/0${ id }/image02.jpg`")
   b-container(v-if='item')
     .interview-content.interview-content--rev
       section.interview-content__section(v-for='(n,index) in 3')
-        h3.interview-content__q
-          |{{question[index +3]}}
+        h3.interview-content__q(v-html="item.question[`${index + 3}`]")
         .interview-content__a(v-html="item.answer[`${index+3}`]")
   .interview-message
     .interview-message__text.interview-message__text01
@@ -36,8 +34,7 @@
   b-container(v-if='item')
       .interview-content
         section.interview-content__section(v-for='(n,index) in 3')
-          h3.interview-content__q
-            |{{question[index +6]}}
+          h3.interview-content__q(v-html="item.question[`${index + 6}`]")
           .interview-content__a(v-html="item.answer[`${index+6}`]")
       .interview-footer
         b-img.interview-footer__img(v-bind:src="`/static/images/member/0${ id }/image03.jpg`")
@@ -61,34 +58,6 @@ export default {
     return {
       item: null,
       num: this.id
-    }
-  },
-  computed: {
-    question: function () {
-      let list = [
-        '出身校と学生のときに打ち込んだことを教えてください。',
-        'アイムファクトリーに入社する前に、「IT」に関する知識はありましたか？',
-        'アイムファクトリーの第一印象は？/会社(会社説明や会社の雰囲気)で、印象に残っていることは？',
-        '就職活動を行うとき優先順位を高くしたものは？/会社の話を聞いていて、これいいなと思ったところは？',
-        'アイムファクトリーに入社した理由は？',
-        'アイムファクトリーってどんな会社？',
-        '仕事で心がけていること　/　自分の「成長したな」と思うところは?',
-        '目標はありますか？',
-        'あなたにとって[社会に変革をもたらすヒトと企業を加速する]ミッションとは？'
-      ]
-      if (this.item.division === 2) {
-        list = [
-          '(職種の確認)お仕事内容を教えてください。/一日のスケジュールを教えてください。',
-          '前職はどんな業界で、どんなお仕事をしていましたか？-ご転職のきっかけは？',
-          'アイムファクトリーの第一印象は？',
-          '転職活動を行うとき優先順位を高くしたものは？/会社の話を聞いていて、これいいなと思ったところは？',
-          'なぜアイムファクトリーに入社しようと思いましたか？',
-          '入社直後/入ってみて、アイムファクトリーをどう感じましたか？',
-          'いまの仕事の課題/目標は？/どんなことを、どんな手段で達成したいですか？',
-          'あなたにとって[社会に変革をもたらすヒトと企業を加速する]ミッションとは'
-        ]
-      }
-      return list
     }
   },
   watch: {
@@ -117,7 +86,6 @@ export default {
     })
   }
 }
-
 </script>
 
 <style lang='scss'>
