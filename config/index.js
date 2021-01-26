b2b0758588160d4232f1bfbf69243ce4d6da83fe
inoue <inoue@inouenoMacBook-Pro-2.local>
2021-01-26 15:50:23 +0900

    build

--- config/index.js
+++ config/index.js
@@ -58,7 +58,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'recruit2022/static',
     assetsPublicPath: './',
 
     /**
